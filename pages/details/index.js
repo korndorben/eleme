a7c61ce8cf725181940d8a3f0dc8fa1b844bee3e
zhang.baokun <zhang.baokun@etao.cnn>
2017-04-21 18:01:18 +0800

    门店列表，商品列表，

---
+++ pages/details/index.js
@@ -0,0 +1,14 @@
+var app = getApp()
+Page({
+    data: {
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1200,
+    },
+
+    onLoad: function (option) {
+
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,25 +2,46 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        suppliers:[]
+    },
+
+    onLoad: function() {
+        console.log('onLoad')
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+
+
+        wx.getLocation({
+            type: 'wgs84',
+            success: function(res) {
+                wx.request({
+                    url: 'https://ct.etao.cn/api/supplier/list',
+                    method: 'get',
+                    data: {
+                        currlongitude: res.longitude,
+                        currlatitude: res.latitude,
+                        type: 2
+                    },
+                    header: {
+                        'Accept': 'application/json'
+                    },
+                    success: function(res) {
+                        that.setData({
+                            suppliers: res.data.data.list
+                        });
+                    }
+                })
+            }
+        })
+
+    }
 })
