93cb0139e6a3f4bb156c40689d23ca1dd1bdc608
张保坤 <zhangbaokun@pku.edu.cn>
2018-03-21 23:37:30 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,30 @@
 var app = getApp()
 Page({
     data: {
+        supplier:{}
     },
 
     onLoad: function() {
+        let that = this;
+        wx.request({
+            url: app.globalData.baseUrl,
+            method: 'post',
+            data: {
+                query: `query querysupplier($supplierid:Int!){ supplier(id: $supplierid) { id stars name logo address hotline } } `,
+                variables: {
+                    supplierid: 1
+                }
+            },
+            header: {
+                'Accept': 'application/json'
+            },
+            success: function(res) {
+                console.log(res)
+                let [supplier] = res.data.data.supplier;
+                that.setData({
+                    supplier: supplier
+                });
+            }
+        })
     }
 })
